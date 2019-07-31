d7cda509f84a2c51bfbba0a85b564bbb02773f83
MasayaYamada <masaya.yamada.stripes@gmail.com>
2019-08-01 08:43:00 +0900

    Initial Commit

---
+++ StarsCommunityNews/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  StarsCommunityNews
+//
+//  Created by Yamada, Masaya on 8/1/19.
+//  Copyright © 2019 Yamada, Masaya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
